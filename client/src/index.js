a48ad9e82998647eeb64be9a4c15a03507d10fd4
ashuthikole2004 <ashwinithikole@gmail.com>
2025-02-09 17:03:03 +0530

    auth problem resolved

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import { applyMiddleware,compose } from 'redux';
-import {legacy_createStore as createstore} from "redux"
-import {thunk} from "redux-thunk"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Reducers from './Reducers';
-const store=createstore(Reducers,compose(applyMiddleware(thunk)));
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose } from "redux";
+import { legacy_createStore as createstore } from "redux";
+import { thunk } from "redux-thunk";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Reducers from "./Reducers";
+const store = createstore(Reducers, compose(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <Provider store={store}>
-    <GoogleOAuthProvider clientId="648063531490-g4gljtu5budsriemcpjiq6rcq6opaf9h.apps.googleusercontent.com">
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </GoogleOAuthProvider>
+    {/* <GoogleOAuthProvider clientId="648063531490-g4gljtu5budsriemcpjiq6rcq6opaf9h.apps.googleusercontent.com"> */}
+    <GoogleOAuthProvider clientId="866775207347-lgc26kerfp6chkrtsagt84382jgvs6ip.apps.googleusercontent.com">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GoogleOAuthProvider>
   </Provider>
 );
 
